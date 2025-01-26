98d8a0c7c3575a407ca1ce88c0e88b265ccc4f05
Ruba Ummmer <rubaummer123@gmail.com>
2025-01-26 10:42:28 +0530

    fix

--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -194,7 +194,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MyHome(),
+                        builder: (context) => MySignIn(),
                       ),
                     );
                   },
